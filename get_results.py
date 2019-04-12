388d75a15b6a730bdb28bb393c3c7343b9c18755
Carson Denison <carsondenison@college.harvard.edu>
2019-04-12 11:04:43 -0400

    all the pieces work for single images

--- get_results.py
+++ get_results.py
@@ -1,6 +1,9 @@
 import boto3
 
 keys = open(file='keys.txt',mode='r').read().splitlines()
+MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+
+
 mturk = boto3.client('mturk',
    aws_access_key_id = keys[0],
    aws_secret_access_key = keys[1],
@@ -15,9 +18,11 @@ mturk = boto3.client('mturk',
 import xmltodict
 
 # Use the hit_id previously created
-hit_id = 'PASTE_IN_YOUR_HIT_ID'
+hit_id_file = 'hit_id.txt'
+hit_id = open(file=hit_id_file, mode='r').read()
 
 # We are only publishing this task to one Worker
 # So we will get back an array with one item if it has been completed
 
 worker_results = mturk.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted'])
+print(worker_results)
