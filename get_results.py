a4b7bf90b66f2dc22b6a06e17343e02b13ab47b5
Carson Denison <carsondenison@college.harvard.edu>
2019-04-17 22:56:15 -0400

    now loops over any files in vega directory

--- get_results.py
+++ get_results.py
@@ -1,28 +1,46 @@
 import boto3
-
-keys = open(file='keys.txt',mode='r').read().splitlines()
-MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
-
-
-mturk = boto3.client('mturk',
-   aws_access_key_id = keys[0],
-   aws_secret_access_key = keys[1],
-   region_name='us-east-1',
-   endpoint_url = MTURK_SANDBOX
-)
-
 # You will need the following library
 # to help parse the XML answers supplied from MTurk
 # Install it in your local environment with
 # pip install xmltodict
 import xmltodict
 
-# Use the hit_id previously created
-hit_id_file = 'hit_id.txt'
-hit_id = open(file=hit_id_file, mode='r').read()
+keys = open(file='keys.txt',mode='r').read().splitlines()
+MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+
+
+mturk = boto3.client('mturk',
+	aws_access_key_id = keys[0],
+	aws_secret_access_key = keys[1],
+	region_name='us-east-1',
+	endpoint_url = MTURK_SANDBOX
+)
+
+# Get a list of the HIT ID's so we can loop through and get all results
+hit_id_file = 'hit_ids.txt'
+hit_ids = ''
+with open(file=hit_id_file, mode='r') as file_contents:
+	hit_ids = file_contents.read().splitlines()
 
 # We are only publishing this task to one Worker
 # So we will get back an array with one item if it has been completed
-
-worker_results = mturk.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted'])
-print(worker_results)
+for i in range(len(hit_ids)):
+	hit_id = hit_ids[i]
+	worker_results = mturk.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted'])
+	print()
+	print("For HIT number " + str(i) + ":")
+	if worker_results['NumResults'] > 0:
+		for assignment in worker_results['Assignments']:
+			xml_doc = xmltodict.parse(assignment['Answer'])
+			print("Worker's answer was:")
+			if type(xml_doc['QuestionFormAnswers']['Answer']) is list:
+				# Multiple fields in HIT layout
+				for answer_field in xml_doc['QuestionFormAnswers']['Answer']:
+					print("For input field: " + answer_field['QuestionIdentifier'])
+					print("Submitted answer: " + answer_field['FreeText'])
+			else:
+				# One field found in HIT layout
+				print("For input field: " + xml_doc['QuestionFormAnswers']['Answer']['QuestionIdentifier'])
+				print("Submitted answer: " + xml_doc['QuestionFormAnswers']['Answer']['FreeText'])
+	else:
+		print("No results ready yet for HIT")
