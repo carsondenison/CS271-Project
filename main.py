94d0a481497bd2a301b0d1077bf816f184f7cd4e
Carson Denison <carsondenison@college.harvard.edu>
2019-04-19 14:10:00 -0400

    wrote rect generator

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
-import numpy as np 
+
+vega_directory = './vega_files/'
+template_directory = './templates/'
+colors  = ['black', 'red', 'blue', 'green', 'orange', 'yellow', 'purple', 'white']
 
 #test matt jiang
-def main():
+def try1():
 	schema = "https://vega.github.io/schema/vega-lite/v3.json"
 	data = "data/cars.json"
 	marks = ["area", "bar", "circle", "line", "point", "rect", "rule", 
@@ -20,4 +23,15 @@ def main():
 					"y": {"field": "Miles_per_Gallon", "type": "quantitative"}
 				}}, outfile)
 
-main()
+def t1():
+	with open(file = template_directory + 't1_template.vl',mode='r') as template_file:
+		template = template_file.read().split('SPLIT_LOCATION')
+		for color in colors:
+			for stroke_width in range(1, 5):
+				with open(file = vega_directory+'t1_'+color+'_'+str(stroke_width),mode='w+') as out:
+					out.write(template[0] + color + template[1] + str(stroke_width) + template[2])
+
+def t2():
+	
+
+t1()
