298f20d7a9c7d8f74a6dce83fce004b37ac8c2b9
Carson Denison <carsondenison@college.harvard.edu>
2019-04-22 17:32:48 -0400

    added new init files

---
+++ gen_init.py
@@ -0,0 +1,38 @@
+# Initializer values and locations for dependencies
+vega_directory = './vega_files/'
+template_directory = './templates/'
+data_location = './data/'
+
+# Change here to filter visualizations
+
+# FOR BAR CHARTS: 
+#------------------------------------------------------------------------------
+
+# Colors for borders, fills are set by data. See vega docs for full set
+colors = ['black', 'blue', 'green', 'purple', 'white']
+
+# Padding between groups of columns
+paddings = [0.0, 0.2, 0.4]
+
+# Width of bar borders
+stroke_widths = [1, 2, 3, 4]
+
+
+
+# FOR SCATTER PLOTS:
+#------------------------------------------------------------------------------
+
+# Size of scatter plot points
+sizes = [10, 50, 100, 200, 300]
+
+
+
+# WHICH PLOTS DO WE WANT:
+#------------------------------------------------------------------------------
+
+scatter = True
+heer_t1 = False
+heer_t2 = False
+heer_t3 = False
+heer_t4 = False
+heer_t5 = False
