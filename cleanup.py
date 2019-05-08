f1876885f18c70184d14e1e3a0ab692718d9abe3
Carson Denison <carsondenison@college.harvard.edu>
2019-05-08 13:30:15 -0400

    Fixed, can't have . in div name

--- cleanup.py
+++ cleanup.py
@@ -2,14 +2,14 @@ import os
 from gen_init import *
 
 # Put the list of visualizations to keep in here, copied from view_vega.html:
-save_list = ['scatter_100.vl']
+save_list = ['scatter_50', 'scatter_200']
 
 if not save_list:
 	print("If you want to remove all your files, use the command line instead")
 else:
 	print("Removing files:")
 	for vega_file in os.listdir(vega_directory):
-		if vega_file not in save_list:
+		if vega_file.split('.')[0] not in save_list:
 			os.remove(vega_directory + vega_file)
 			print("removed: " + vega_directory + vega_file)
 	print()
