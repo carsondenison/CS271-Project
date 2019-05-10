625a10430e62be110fceb26e63dc0393e4d9d261
Matt Jiang <linghuajiang@college.harvard.edu>
2019-05-10 11:52:31 -0400

    changed website button

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         
         // Check if there are selected checkboxes
         if(selected.length > 0){
-            alert("Selected checkboxes value: ['" + selected + "']");	
+            alert("Enter this as argument for cleanup.py: ['" + selected + "']");	
         }else{
             alert("Please select at least one checkbox.");	
         }
